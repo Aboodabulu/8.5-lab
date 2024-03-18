239ab9bb84333bc9f6e195f977ad93f924df56b3
Aboodabulu <146123418+Aboodabulu@users.noreply.github.com>
2024-03-18 22:08:23 +0000

    initial-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://kiknuotgjjqsbslznsve.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtpa251b3Rnampxc2JzbHpuc3ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTYwNTIsImV4cCI6MjAyNjM3MjA1Mn0.uTgR4JQfV-PhiixdCwO1nnSO0kWwIWhCAktJejncp9g'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getBooks(){
+  let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+  for (let book of books) {
+    let bookList = document.getElementById('books');
+    bookList.innerHTML += `<li>${book.title}</li>`
+  }
+}
+
+getBooks();
